76ddee3fe94801f0913f5648e3af887c8bd8c7ea
Fang-Pen Lin <bornstub@gmail.com>
2017-02-15 16:57:20 -0800

    Init project

---
+++ setup.py
@@ -0,0 +1,9 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+setup(
+    name='ibeacon_api',
+    version='1.0.0',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['flask'],
+)
