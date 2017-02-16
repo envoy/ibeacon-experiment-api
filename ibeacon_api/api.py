b526ced6662f86bdbd6df10b7005a87ab0eea2ab
Fang-Pen Lin <bornstub@gmail.com>
2017-02-15 17:04:26 -0800

    Add TODOs for the API

--- ibeacon_api/api.py
+++ ibeacon_api/api.py
@@ -1,9 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route("/sign-up")
+def sign_up():
+    # TODO: create user record for iPhone
+    return 'FIXME'
+
+
+@app.route("/upload-log")
+def upload_log():
+    # TODO: handle uploaded log file from iPhone
+    return 'FIXME'
+
+
+@app.route("/i-am-here")
+def i_am_here():
+    # TODO: create an i am here record from iPad
+    return 'FIXME'
+
 
 if __name__ == "__main__":
     app.run()
